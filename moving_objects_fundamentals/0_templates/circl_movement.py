8365401b61a948c1797f6a1b4b993a4ac82d2861
Ghasak Ibrahim <mdotgh@gmail.com>
2019-06-18 01:18:50 +0900

    Added moving_objects_fundamentals

--- moving_objects_fundamentals/0_templates/circl_movement.py
+++ moving_objects_fundamentals/0_templates/circl_movement.py
@@ -1,143 +1,57 @@
-"""
- Move a sprite in a circle.
+# website: codeblockgetter.wordpress.com
 
- Sample Python/Pygame Programs
- Simpson College Computer Science
- http://programarcadegames.com/
- http://simpson.edu/computer-science/
+from pygame.locals import *
+import pygame, sys
 
-"""
+pygame.init() # Initializing the pygame module
 
-import pygame
-import random
-import math
-
-# Colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-
-
-class Block(pygame.sprite.Sprite):
-    """ This class represents the ball that moves in a circle. """
-
-    def __init__(self, color, width, height):
-        """ Constructor that create's the ball's image. """
-        super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
-
-        # The "center" the sprite will orbit
-        self.center_x = 0
-        self.center_y = 0
-
-        # Current angle in radians
-        self.angle = 0
-
-        # How far away from the center to orbit, in pixels
-        self.radius = 0
-
-        # How fast to orbit, in radians per frame
-        self.speed = 0.05
-
-    def update(self):
-        """ Update the ball's position. """
-        # Calculate a new x, y
-        self.rect.x = self.radius * math.sin(self.angle) + self.center_x
-        self.rect.y = self.radius * math.cos(self.angle) + self.center_y
-
-        # Increase the angle in prep for the next round.
-        self.angle += self.speed
-
-
-class Player(pygame.sprite.Sprite):
-    """ Class to represent the player. """
-    def __init__(self, color, width, height):
-        """ Create the player image. """
-        super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
-
-    def update(self):
-        """Set the user to be where the mouse is. """
-        pos = pygame.mouse.get_pos()
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
-
-# Initialize Pygame
-pygame.init()
-
-# Set the height and width of the screen
-SCREEN_WIDTH = 700
-SCREEN_HEIGHT = 400
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-
-# This is a list of 'sprites.' Each block in the program is
-# added to this list. The list is managed by a class called 'Group.'
-block_list = pygame.sprite.Group()
-
-# This is a list of every sprite. All blocks and the player block as well.
-all_sprites_list = pygame.sprite.Group()
-
-for i in range(50):
-    # This represents a block
-    block = Block(BLACK, 20, 15)
-
-    # Set a random center location for the block to orbit
-    block.center_x = random.randrange(SCREEN_WIDTH)
-    block.center_y = random.randrange(SCREEN_HEIGHT)
-    # Random radius from 10 to 200
-    block.radius = random.randrange(10, 200)
-    # Random start angle from 0 to 2pi
-    block.angle = random.random() * 2 * math.pi
-    # radians per frame
-    block.speed = 0.008
-    # Add the block to the list of objects
-    block_list.add(block)
-    all_sprites_list.add(block)
-
-# Create a RED player block
-player = Player(RED, 20, 15)
-all_sprites_list.add(player)
-
-# Loop until the user clicks the close button.
-done = False
-
-# Used to manage how fast the screen updates
+FPS = 60
 clock = pygame.time.Clock()
 
-score = 0
+# RGB
+WHITE = (255, 255, 255)
+BLUE =  (  0,   0, 255)
+GREEN = (  0, 255,   0)
 
-# -------- Main Program Loop -----------
-while not done:
+rect1_x = 290
+rect1_y = 190
+rect1_size_x = 20
+rect1_size_y = 20
+
+rect1_x_change = 0
+rect1_y_change = 0
+
+rect2_x = 500
+rect2_y = 150
+rect2_size_x = 100
+rect2_size_y = 100
+
+window = pygame.display.set_mode((600, 400))
+window.fill(WHITE)
+
+color = WHITE
+
+rect1 = pygame.draw.rect(window, BLUE, [rect1_x, rect1_y, rect1_size_x, rect1_size_y])
+rect2 = pygame.draw.rect(window, color, [rect2_x, rect2_y, rect2_size_x, rect2_size_y])
+
+while True: # main game loop
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+        if event.type == QUIT: # QUIT
+            pygame.quit
+            sys.exit()
 
-    all_sprites_list.update()
+    window.fill(WHITE)
 
-    # Clear the screen
-    screen.fill(WHITE)
+    if rect1_x+20 == rect2_x:
+        color = GREEN
+        rect1_x_change = 0
+    else:
+        rect1_x_change = 5
 
-    # See if the player block has collided with anything.
-    blocks_hit_list = pygame.sprite.spritecollide(player, block_list, True)
+    rect1_x += rect1_x_change
 
-    # Check the list of collisions.
-    for block in blocks_hit_list:
-        score += 1
-        print( score )
+    rect1 = pygame.draw.rect(window, BLUE, [rect1_x, rect1_y, rect1_size_x, rect1_size_y])
+    rect2 = pygame.draw.rect(window, color, [rect2_x, rect2_y, rect2_size_x, rect2_size_y])
 
-    # Draw all the spites
-    all_sprites_list.draw(screen)
-
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
-
-    # Limit to 60 frames per second
-    clock.tick(60)
-
-pygame.quit()
+    clock.tick(FPS) # changes the frames per 60 sec
+    pygame.display.flip() # updates the display
